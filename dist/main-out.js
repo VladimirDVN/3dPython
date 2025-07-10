f339848ca4fcff7149454fbc18b96de9ab778ecc
VladimirDVN <37466011+VladimirDVN@users.noreply.github.com>
2025-07-10 10:11:09 +0300

    Add files via upload

--- dist/main-out.js
+++ dist/main-out.js
@@ -62004,14 +62004,18 @@ model.rotation.set( 0, -Math.PI/2, 0 );
         // Добавление сферы как дочернего объекта к кубу
 /* parentCylinder.add(childSphere); */
 parentCylinder.add(model);
-
+parentCylinder.rotation.set( Math.PI/2, -Math.PI/2, 0 ); 
 //wormSegments.push(createSegmentB(0xff0000,0.8*cellWidth,cellWidth,texture));   //Head of a snake
 wormSegments.push(parentCylinder);
-
+let body;
 for (let i = 1; i < STARTING_NUM_SEGMENTS-1; i++) {
-    wormSegments.push(createSegmentB(0x00ff00,cellWidth,cellWidth,textureB)); // Green body
-        }
-wormSegments.push(createSegmentB(0x00ff00,cellWidth,0.0*cellWidth,textureB)); // tail
+	body = createSegmentB(0x00ff00,cellWidth,cellWidth,textureB);
+	body.rotation.set(Math.PI/2, 0, 0);	
+    wormSegments.push(body); // Green body
+}
+let tail = createSegmentB(0x00ff00,cellWidth,0.0*cellWidth,textureB);
+tail.rotation.set(Math.PI/2, 0, 0);	
+wormSegments.push(tail); // tail
 wormSegments.forEach(segment => scene.add(segment));		
 
 let directionalLight = new DirectionalLight(0xffffff, 3,854 );
@@ -62056,7 +62060,7 @@ function setUpState() {
 	}    
 	
 	for (let i = 0; i < STARTING_NUM_SEGMENTS; i++) {
-		let tmp2 = new Vector3(0, -i * cellWidth, 0);		//(0, 0, cellWidth -i * cellWidth)
+		let tmp2 = new Vector3(0, 0, cellWidth -i * cellWidth);		//(	0, -i * cellWidth, 0)
 		segments.push(tmp2);
     }
 	newHeadPos = segments[0].clone();
@@ -62232,8 +62236,8 @@ function calRotation(direction) {
 	let newDir;
 	if(direction.z == -1) newDir = new Vector3(-1,0,0);
 	if(direction.z == 1)	newDir = new Vector3(1,0,0);
-	if(direction.y == -1)	newDir = new Vector3(2,0,0);
-	if(direction.y == 1)	newDir = new Vector3(0,0,0);
+	if(direction.y == -1)	newDir = new Vector3(0,-1,0);
+	if(direction.y == 1)	newDir = new Vector3(0,1,0);
 	if(direction.x == -1)	newDir = new Vector3(0,0,1);
 	if(direction.x == 1)	newDir = new Vector3(0,0,-1);
 	if(direction.equals(zeroVector)) newDir = new Vector3(0,0,0);
@@ -62247,10 +62251,11 @@ function drawSnake() {
 		let seg = wormSegments[i];
 		seg.position.set(segments[i].x,segments[i].y,segments[i].z);
 		if(i == 0) {
-			if(zeroVector !== newDir) {
-				seg.rotation.set(0,0,0);
+			if(!(zeroVector.equals(newDir))) {
+//				seg.rotation.set(0,0,0);
 				let rr = calRotation(newDir);
 				seg.rotation.set(rr.x*Math.PI / 2,rr.y*Math.PI / 2,rr.z*Math.PI / 2);
+				newDir = new Vector3(0,0,0);
 			}
 			scene.add(seg);
 //			const bottomLocal = new THREE.Vector3(segments[i].x,segments[i].y,segments[i].z);
